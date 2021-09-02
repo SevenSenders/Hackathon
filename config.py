40253d5cd82e97466c1e1d948c3727a30360cbdb
Ensieh Rahbar <78486625+e-rahbar@users.noreply.github.com>
2021-09-02 13:42:56 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+extensions = ['sphinxcontrib.confluencebuilder']
+confluence_publish = True
+confluence_space_name = 'Data Hub'
+confluence_parent_page = 'Development'
+confluence_server_url = '	https://sevensenders.atlassian.net/wiki/x/ohFlAw'
+confluence_ask_password = True
+confluence_page_hierarchy = True
